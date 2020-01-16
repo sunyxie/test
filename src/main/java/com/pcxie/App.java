0d49088c067995d421c383b381637d57a1134824
xiepengcheng <xiepengcheng61755185@gmail.com>
2020-01-16 21:22:32 +0800

    Initial commit

---
+++ src/main/java/com/pcxie/App.java
@@ -0,0 +1,13 @@
+package com.pcxie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
